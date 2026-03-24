6dadc7b53e6bcb07cc45415bf7fce1d09787a71f
Marie Joelle Ndioukane <mariejoelleisabelle9506@gmail.com>
2026-03-24 15:09:42 -0400

    project tracking device

---
+++ dev/project_tracking_device.py
@@ -0,0 +1,19 @@
+import unittest
+
+class ProjectTrackingDevice():
+
+    def test_tracking_manager() :
+        pass
+
+    def test_tracking_device():
+        pass
+
+    def test_trigger_device():
+        pass
+
+    def test_tracking_application():
+        pass
+    
+
+if __name__ == "__main__":
+    unittest.main()
