3c7f6db1e36de4a713d20daf9faae11b9fcdc668
Julien <jlamontagne5606@gmail.com>
2026-05-07 18:53:15 -0400

    les states sont valides

--- dev/scouter_state_machine.py
+++ dev/scouter_state_machine.py
@@ -1,6 +1,6 @@
 from typing import override
 from state_machine_device import StateMachineDevice
-from state_machine_utilities import MonitoredState, State, DelaySinceEnteredCondition
+from state_machine_utilities import MonitoredState, ActionState, State, DelaySinceEnteredCondition, ConditionalTransition
 from condition import Condition, ReaderCondition, AllConditions, AnyConditions, AbstractValueCondition
 from tracking_device import TrackingApplication
 from blinker_device import BlinkerDevice
@@ -50,8 +50,13 @@ class Charging(State):
         def __init__(self, console):
             pass
 
-    def __init__(self, console: Console):
+    def __init__(self, name, console: Console):
         battery_management = Charging.BatteryManagement(console)
+        super().__init__(name)
+
+    @override
+    def _do_entering_action(self):
+        print("CHARGING")
 
 class Scooting(State):
     
@@ -59,8 +64,13 @@ class Scooting(State):
         def __init__(self, console: Console):
             pass
 
-    def __init__(self, console):
+    def __init__(self, name, console):
         ride_management = Scooting.RideManagement(console)
+        super().__init__(name)
+    
+    @override
+    def _do_entering_action(self):
+        print("SCOOTING")
 
 class ScooterStateMachine(StateMachineDevice):
     def __init__(self, console):
@@ -68,7 +78,7 @@ class ScooterStateMachine(StateMachineDevice):
         self.__plugged_in = False
 
         #States
-        power_off_state = State("power_off")
+        power_off_state = ActionState("power_off")
         unlocking_state = MonitoredState("unlocking")
         powering_up_state = MonitoredState("powerring_up")
         idle_state = MonitoredState("idle")
@@ -86,26 +96,46 @@ class ScooterStateMachine(StateMachineDevice):
         speed_less_then_condition = LessThenCondition(0.01, lambda:self.__scooter.speed)
 
         #Transitions
-        power_off_state.add_transition(plugged_in_condition, charging_state)
-        power_off_state.add_transition(AllConditions([KeyPressCondition(console, "P"), plugged_out_condition]), unlocking_state)
-        charging_state.add_transition(ReaderCondition(False, lambda:self.__plugged_in), power_off_state)
+        power_off_state.add_transition(ConditionalTransition(plugged_in_condition, unlocking_state))
+        power_off_state.add_transition(ConditionalTransition(AllConditions([KeyPressCondition(console, "P"), plugged_out_condition]), unlocking_state))
+        charging_state.add_transition(ConditionalTransition(ReaderCondition(False, lambda:self.__plugged_in), power_off_state)) 
         #À vérifier si ya meilleure solution
-        charging_state.add_transition(ReaderCondition(True, lambda:charging_state.BatteryManagement.current_state.terminal), charging_failed_state)
-        charging_failed_state.add_transition(DelaySinceEnteredCondition(3.0, charging_failed_state), powering_down_state)
-        unlocking_state.add_transition(NotKeyPressCondition(console, "P"), power_off_state)
-        unlocking_state.add_transition(DelaySinceEnteredCondition(3.0, unlocking_state), powering_up_state)
-        powering_up_state.add_transition(DelaySinceEnteredCondition(3.0, powering_up_state), idle_state)
-        powering_up_state.add_transition(KeyPressCondition(console, "F"), intygrity_failed_state)
-        intygrity_failed_state.add_transition(DelaySinceEnteredCondition(3.0, intygrity_failed_state), powering_down_state)
-        idle_state.add_transition(KeyPressCondition(console, "P"), locking_state)
-        idle_state.add_transition(AnyConditions([DelaySinceEnteredCondition(30.0, idle_state), ]), powering_down_state)
-        idle_state.add_transition(power_less_then_condition, powering_down_state)
-        idle_state.add_transition(KeyPressCondition(console, "A"), scooting_state)
-        scooting_state.add_transition(AnyConditions([power_less_then_condition, speed_less_then_condition], idle_state))
-        locking_state.add_transition(NotKeyPressCondition(console, "P"), idle_state)
-        locking_state.add_transition(DelaySinceEnteredCondition(3.0, locking_state), powering_down_state)
-        powering_down_state.add_transition(DelaySinceEnteredCondition(3.0, powering_down_state), power_off_state)
+        charging_state.add_transition(ConditionalTransition(ReaderCondition(True, lambda:charging_state.BatteryManagement.current_state.terminal), charging_failed_state)) 
+        charging_failed_state.add_transition(ConditionalTransition(DelaySinceEnteredCondition(3.0, charging_failed_state), powering_down_state)) 
+        unlocking_state.add_transition(ConditionalTransition(NotKeyPressCondition(console, "P"), power_off_state))
+        unlocking_state.add_transition(ConditionalTransition(DelaySinceEnteredCondition(3.0, unlocking_state), powering_up_state))
+        powering_up_state.add_transition(ConditionalTransition(DelaySinceEnteredCondition(3.0, powering_up_state), idle_state))
+        powering_up_state.add_transition(ConditionalTransition(KeyPressCondition(console, "F"), intygrity_failed_state))
+        intygrity_failed_state.add_transition(ConditionalTransition(DelaySinceEnteredCondition(3.0, intygrity_failed_state), powering_down_state))
+        idle_state.add_transition(ConditionalTransition(KeyPressCondition(console, "P"), locking_state))
+        idle_state.add_transition(ConditionalTransition(AnyConditions([DelaySinceEnteredCondition(30.0, idle_state), ]), powering_down_state))
+        idle_state.add_transition(ConditionalTransition(power_less_then_condition, powering_down_state))
+        idle_state.add_transition(ConditionalTransition(KeyPressCondition(console, "A"), scooting_state))
+        scooting_state.add_transition(ConditionalTransition(AnyConditions([power_less_then_condition, speed_less_then_condition]), idle_state)) 
+        locking_state.add_transition(ConditionalTransition(NotKeyPressCondition(console, "P"), idle_state)) 
+        locking_state.add_transition(ConditionalTransition(DelaySinceEnteredCondition(3.0, locking_state), powering_down_state)) 
+        powering_down_state.add_transition(ConditionalTransition(DelaySinceEnteredCondition(3.0, powering_down_state), power_off_state))
 
+        #Actions
+        power_off_state.add_entering_action(self._on_power_off)
+        unlocking_state.add_entering_action(self._on_unlocking)
+        powering_up_state.add_entering_action(self._on_powering_up)
+        powering_down_state.add_entering_action(self._on_powering_down)
+        idle_state.add_entering_action(self._on_idle)
+        intygrity_failed_state.add_entering_action(self._on_integrity_failed)
+        locking_state.add_entering_action(self._on_locking)
+        charging_failed_state.add_entering_action(self._on_charing_failed)
+
+        # print(f'power_off: {power_off_state.valid}')
+        # print(f'unlocking: {unlocking_state.valid}')
+        # print(f'powering_up: {powering_up_state.valid}')
+        # print(f'powering_down: {powering_down_state.valid}')
+        # print(f'idle: {idle_state.valid}')
+        # print(f'charging: {charging_state.valid}')
+        # print(f'charging_failed: {charging_failed_state.valid}')
+        # print(f'scooting: {scooting_state.valid}')
+        # print(f'locking: {locking_state.valid}')
+        # print(f'intygrity_failed: {intygrity_failed_state.valid}')
 
         layout = (power_off_state, 
                   unlocking_state, 
@@ -120,12 +150,30 @@ class ScooterStateMachine(StateMachineDevice):
         
         super().__init__(StateMachineDevice.Layout(layout))
 
+    def _on_power_off(self):
+        print("POWER OFF")
+    def _on_unlocking(self):
+        print("UNLOCKING")
+    def _on_powering_up(self):
+        print("POWERING ON")
+    def _on_idle(self):
+        print("IDLE")
+    def _on_powering_down(self):
+        print("POWERING DOWN")
+    def _on_integrity_failed(self):
+        print("INTEGRITY FAILED")
+    def _on_locking(self):
+        print("LOCKING")
+    def _on_charing_failed(self):
+        print("CHARGING FAILED")
+
 def main():
     console = Console()
     app = TrackingApplication()
     scooter = ScooterStateMachine(console)
 
     app.add_device(scooter)
+    app.run_forever()
 
 if __name__ == "__main__":
     main()
