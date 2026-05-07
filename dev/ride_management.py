ec6873f239620ada1df1c5cc72d41493427d3f71
JeremieP12 <djepro4@gmail.com>
2026-05-07 12:39:26 -0400

    Add: ride_management, scooter

--- dev/ride_management.py
+++ dev/ride_management.py
@@ -1,28 +1,45 @@
-from typing import Self
+from typing import Self, override
 from state_machine_device import StateMachineDevice, State
 from state_machine_utilities import ConditionalTransition, ActionState
-from condition import ValueCondition
+from condition import ReaderCondition
 from console import Console
+from scooter import Scooter
 
 class RideManagement(StateMachineDevice):
-    Pi = 2.5
-    Pm = 120.0
-    Er = 120.0
-    Aa = 0.75
-    Ab = 0.5
-
-    def __init__(self:Self ,console:Console, initialized:bool = False, name:str = None, enabled:bool = True) -> None:
-        
-        self.__free_wheel_state = State("Free Wheel")
+    def __init__(self:Self,console:Console, scooter:Scooter, initialized:bool = False, name:str = None, enabled:bool = True) -> None:
+        self.__delta_time = 0
+        self.__scooter = scooter
+        self.__console = console
+        self.__free_wheel_state = ActionState("Free Wheel")
         self.__accelerating_state = ActionState("Accelerating")
-        self.__breaking_state = State("Breaking")
+        self.__breaking_state = ActionState("Breaking")
 
-        self.__free_wheel_state.add_transition(ConditionalTransition(ValueCondition(console.SpecialKey.UP_ARROW,console.key_pressed),self.__accelerating_state))
-        self.__free_wheel_state.add_transition(ConditionalTransition(ValueCondition(console.SpecialKey.DOWN_ARROW,console.key_pressed),self.__breaking_state))
-        self.__accelerating_state.add_transition(ConditionalTransition(ValueCondition(console.SpecialKey.DOWN_ARROW,console.key_pressed),self.__breaking_state))
+        self.__free_wheel_state.add_transition(ConditionalTransition(ReaderCondition(True, self.__read_up_arrow),self.__accelerating_state))
+        self.__free_wheel_state.add_transition(ConditionalTransition(ReaderCondition(True, self.__read_down_arrow),self.__breaking_state))
+        self.__accelerating_state.add_transition(ConditionalTransition(ReaderCondition(True, self.__read_down_arrow),self.__breaking_state))
+        self.__accelerating_state.add_transition(ConditionalTransition(ReaderCondition(False, self.__read_up_arrow), self.__free_wheel_state))
+        self.__breaking_state.add_transition(ConditionalTransition(ReaderCondition(False, self.__read_down_arrow), self.__free_wheel_state))
 
-        self.__accelerating_state.add_in_state_action()
+        self.__accelerating_state.add_in_state_action(self.__accelerate)
+        self.__breaking_state.add_in_state_action(self.__decelerate)
+        self.__free_wheel_state.add_in_state_action(self.__decelerate)
 
         layout = self.Layout((self.__free_wheel_state, self.__accelerating_state, self.__breaking_state))
         super().__init__(layout, initialized, name, enabled)
 
+
+    @override
+    def _do_tracking(self:Self, elapsed_time: float) -> None:
+        self.__delta_time = elapsed_time
+        
+    def __read_up_arrow(self:Self) -> bool:
+        return True if self.__console.SpecialKey.UP_ARROW in self.__console.actual_key_pressed else False
+    
+    def __read_down_arrow(self:Self) -> bool:
+        return True if self.__console.SpecialKey.UP_ARROW in self.__console.actual_key_pressed else False
+
+    def __accelerate(self:Self) -> None:
+        self.__scooter.accelerate(self.__delta_time)
+
+    def __decelerate(self:Self) -> None:
+        self.__scooter.decelerate(self.__delta_time)
---
+++ dev/scooter.py
@@ -0,0 +1,70 @@
+from typing import Self
+
+class Scooter():
+    Pi = 2.5
+    Pm = 120.0
+    Er = 120.0
+    Aa = 0.75
+    Ab = 0.5
+    Ka = 1.1
+    Vv = 15.0
+    Ca = 0.0037
+    Cr = 0.0153
+    Kb = 5.0
+
+    def __init__(self:Self):
+        self.__speed = 0
+        self.__battery = Battery()
+
+    @property
+    def speed(self:Self) -> float:
+        return self.__speed
+    
+    @speed.setter
+    def speed(self:Self, value:float) -> None:
+        if not isinstance(value, float):
+            raise TypeError("Speed must be a float")
+        elif value < 0:
+            raise ValueError("Speed must be greater than 0")
+        else:
+            self.__speed = value
+
+    @property
+    def battery(self:Self) -> Battery:
+        return self.__battery
+    
+    def accelerate(self:Self, delta_time:float) -> None:
+        self.speed += delta_time*(Scooter.Ka*Scooter.Aa*(1-self.speed/Scooter.Vv)-Scooter.Ca*self.speed**2)
+
+    def decelerate(self:Self, delta_time:float) -> None:
+        self.speed = max(0, self.speed - delta_time*(Scooter.Cr + Scooter.Kb*Scooter.Ab + Scooter.Ca*self.speed**2))
+
+
+class Battery():
+    def __init__(self:Self):
+        self.__temperature = 0
+        self.__power = 0
+
+    @property
+    def temperature(self:Self) -> float:
+        return self.__temperature
+
+    @temperature.setter
+    def temperature(self:Self, value:float) -> None:
+        if not isinstance(value, float):
+            raise TypeError("Battery temperature must be a float")
+        else:
+            self.__temperature = value
+
+    @property
+    def power(self:Self) -> float:
+        return self.__power
+
+    @power.setter
+    def power(self:Self, value:float) -> None:
+        if not isinstance(value, float):
+            raise TypeError("Battery charge must be a float")
+        elif value < 0 or value > 100:
+            raise ValueError("Battery charge must be between 0 and 100")
+        else:
+            self.__power = value
